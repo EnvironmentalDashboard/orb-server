7edfafc7793cfebd163a3ea8c0d311d9afed5d40
jeremy <jeremy@jfein.net>
2017-01-06 10:07:11 -0500

    Added LifX authentication; CSS security

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,20 +7,34 @@
 let querystring = require('querystring'),
     request = require('request');
 
+let lifx_api = "https://cloud.lifx.com/oauth";
+
 let authentication = {
+
     /**
      * Redirects client to LifX for authentication
      */
     authenticate: function (req, res) {
 
+        /**
+         * Random, unguessable string to prevent CSS attacks: base64 encodes the
+         * timestamp multipled by a psuedorandom decimal (0-1) and removes non-
+         * alphanumerics
+         * @type {String}
+         */
+        let state = (Buffer.from(''+(Math.random() * +new Date())).toString('base64'))
+            .replace(/[^0-9a-z]/gi, '');
+
+        req.session.request_state = state;
+
         let query = querystring.stringify({
             client_id: process.env.LIFX_CLIENT_ID,
             scope: 'remote_control:all',
-            state: 'state', //@TODO prevent CSS attacks
+            state: state, //@TODO prevent CSS attacks
             response_type: 'code'
         });
 
-        res.redirect('https://cloud.lifx.com/oauth/authorize?' + query);
+        res.redirect(lifx_api + '/authorize?' + query);
     },
 
     /**
@@ -44,8 +58,24 @@ let authentication = {
         /**
          * Request the access token
          */
-        request.post('https://cloud.lifx.com/oauth/token', options, function (err, res, bod) {
-            console.log(bod);
+        request.post(lifx_api + '/token', options, function (err, res, bod) {
+            console.log(req.session.request_state);
+
+            /**
+             * Reject the token if client has incorrect state parameter (see line
+             * 25)
+             */
+            if (req.session.request_state != req.query.state) {
+                return ;
+            }
+
+            let token = bod.access_token;
+
+            console.log(token);
+
+            //req.session.access_token
+
+            //if (Services.recognition.knows(token))
         });
     }
 };
