7f676be75ac56e0672c3ffb632a7e19179b4c091
jeremy <jeremy@jfein.net>
2017-01-16 14:53:27 -0600

    update shell script

--- exe/relative-usage.php
+++ exe/relative-usage.php
@@ -2,11 +2,11 @@
 <?php
 /**
  * This shell is used to delegate the relative usage calculation to the external
- * Gauge class
+ * Meter class
  */
 
-require __DIR__ . '/../../includes/class.Gauge.php';
 require __DIR__ . '/../../includes/db.php';
+require __DIR__ . '/../../includes/class.Meter.php';
 
 // ..$id, $daySets, $start, $end
 $id = $argv[1];
@@ -14,17 +14,7 @@ $daySets = $argv[2];
 $start = $argv[3];
 $end = $argv[4];
 
-$gauge = new Gauge($db);
+$meter = new Meter($db);
+echo $meter->relativeValueOfMeterFromTo($id, $daySets, $start, $end);
 
-$stmt = $db->prepare('SELECT id, current, units FROM meters WHERE id = ?');
-$stmt->execute([$id]);
-
-$meter = $stmt->fetch();
-
-$data = $gauge->filterArray(
-    $gauge->getData($id, $start, $end),
-    $daySets
-);
-
-echo $gauge->relativeValue($data, $meter['current']);
 ?>
