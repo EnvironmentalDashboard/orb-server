8fc3ad6f5d6e26051c35cb7ee40ec0806e0d8842
jeremy <jeremy@jfein.net>
2017-01-16 15:03:22 -0600

    debugging rel. usage

--- exe/relative-usage.php
+++ exe/relative-usage.php
@@ -9,10 +9,10 @@ require __DIR__ . '/../../includes/db.php';
 require __DIR__ . '/../../includes/class.Meter.php';
 
 // ..$id, $daySets, $start, $end
-$id = $argv[1];
-$daySets = $argv[2];
-$start = $argv[3];
-$end = $argv[4];
+$id = 388;//$argv[1];
+$daySets = '[1,2,3,4,5,6,7]';//$argv[2];
+$start = strtotime('-2 weeks');//$argv[3];
+$end = time();//$argv[4];
 
 $meter = new Meter($db);
 echo $meter->relativeValueOfMeterFromTo($id, $daySets, $start, $end);
