f26df2e4ec6bd937a8f4259fceb9fecf74f947d7
jeremy <jeremy@jfein.net>
2017-04-25 22:21:24 -0400

    core user entity (references `users` table)

---
+++ src/model/entities/CoreUser.js
@@ -0,0 +1,12 @@
+/**
+ * @overview Core user entitiy
+ *
+ * This refers to users in the `users` table and NOT the `orb-server_users` table
+ */
+
+let Building = {
+    tableName: 'users',
+    idAttribute: 'id'
+};
+
+module.exports = Building;
