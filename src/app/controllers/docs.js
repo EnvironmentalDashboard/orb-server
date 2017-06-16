381fb35f6a94075ebf6d86720efb6bcf44152c2b
jeremy <jeremy@jfein.net>
2017-06-16 14:39:04 -0500

    documentation modifications

--- src/app/controllers/docs.js
+++ src/app/controllers/docs.js
@@ -2,7 +2,7 @@ let Service = require('../../model/services');
 
 let docsController = {
     index: function(req, appmodel) {
-        return appmodel.setPage(req.params.page).catch(appmodel.setErrors.bind(appmodel));
+        return appmodel.setSlug(req.params.page).catch(appmodel.setErrors.bind(appmodel));
     },
 };
 
