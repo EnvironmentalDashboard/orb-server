2b3268d95e614d7109988f9c8a5d4d3c240c591f
jeremy <jeremy@jfein.net>
2017-06-22 12:20:54 -0500

    Adding integration to account page

--- src/app/modelviews/account.js
+++ src/app/modelviews/account.js
@@ -9,8 +9,16 @@ var account = {
 
     getInputs: function() {
         return this.inputs;
+    },
+
+    retrieveIntegrationList: function() {
+        return Service.BulbIntegrationList.retrieve(this.session).catch(this.setErrors.bind(this));
+    },
+
+    retrieveBuildingDataIntegration: function() {
+        return Service.BuildingDataIntegration.retrieve(this.session).catch(this.setErrors.bind(this));
     }
-    
+
 };
 
 module.exports = account;
--- src/app/views/account.js
+++ src/app/views/account.js
@@ -1,13 +1,48 @@
 let accountView = {
     index: function(res, appmodel) {
-        let loggedIn = appmodel.getAuthenticatedUser();
+        let loggedIn = appmodel.getAuthenticatedUser(),
+            bulbIntegrationListPromise = appmodel.retrieveIntegrationList(),
+            buildingDataIntegrationPromise = appmodel.retrieveBuildingDataIntegration();
 
-        return res.render('account', {
-            loggedIn: loggedIn,
-            page: {
-                active: { account:true },
-                title: "Account Overview"
+        return Promise.all([bulbIntegrationListPromise, buildingDataIntegrationPromise]).then(function(results) {
+            if (appmodel.getAuthError()) {
+                return res.render('denied');
             }
+
+            [integrationList, buildingDataIntegration] = results;
+
+            /**
+             * Building Data Integration Processing
+             */
+            let buildingsAPI = {};
+
+            if(buildingDataIntegration && buildingDataIntegration.related) {
+                buildingsAPI.username = buildingDataIntegration.relations.API.attributes.username;
+            }
+
+            /**
+             * Bulb Integration processing
+             */
+            let integrations = [];
+
+            integrationList.forEach(function(integration){
+                integrations.push({
+                    id: integration.attributes.id,
+                    label: integration.attributes.label || false,
+                    type: integration.attributes.type,
+                    requiresAuth: integration.attributes.status !== 1,
+                });
+            });
+
+            return res.render('account', {
+                loggedIn: loggedIn,
+                integrations: integrations,
+                buildingsAPI: buildingsAPI,
+                page: {
+                    active: {account:true},
+                    title: "Account Overview"
+                }
+            });
         });
     },
 
