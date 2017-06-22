7b94fac74257b54ad946b2242ff233bfb80e61ce
jeremy <jeremy@jfein.net>
2017-06-22 12:21:23 -0500

    Delete integration page

--- src/app/controllers/integration.js
+++
@@ -1,9 +0,0 @@
-let Service = require('../../model/services');
-
-let integrationController = {
-    index: function(req, appmodel) {
-        return Service.Recognition.refreshClient(req.session).catch(appmodel.setErrors.bind(appmodel));
-    }
-};
-
-module.exports = integrationController;
