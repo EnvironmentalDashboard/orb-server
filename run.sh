ae4114004aa2f91488ffa2b1621262c790caecfc
Oliver Ripps <oliver.ripps@gmail.com>
2020-05-31 23:22:06 -0400

    fixed new code review requests

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dit -p 3000:3000 -e HTTPS=0 --restart always --env "DB_HOST" --env "DB_PORT" --env "DB_USER" --env "DB_PWD" --env "DB_NAME" --env-file src/app/config/orb-server.env -v $(pwd)/src:/orb-server/src orb-server
+docker run -dit -p 3000:3000 -e HTTPS=0 --restart always  --env-file src/app/config/orb-server.env -v $(pwd)/src:/orb-server/src orb-server
